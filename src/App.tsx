b78c39349900471933a630489a1ea3b2c7b0939c
David Matoska <david.matoska@gmail.com>
2023-08-06 21:31:08 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import React, {FunctionComponent, useState, useEffect} from 'react';
-import Article from './models/article';
-import ARTICLES from './models/mock-ticket';
-import ArticleList from './pages/article-list';
-  
-const App: FunctionComponent = () => {
- const [articles, setArticles] = useState<Article[]>([]);
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
- useEffect (() => {setArticles(ARTICLES);
-}, []);
-    
- return (
-    <ArticleList />
- )
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-  
+
 export default App;
