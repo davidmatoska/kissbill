93919e359c0b2afa6ebf81e1e522058646e4311a
David Matoska <david.matoska@gmail.com>
2023-08-10 21:59:48 +0200

    commit

--- src/components/AddArticle.tsx
+++ src/components/AddArticle.tsx
@@ -1,5 +1,25 @@
 const AddArticle = () => {
-    return <div>Ajout Article</div>;
+    return <> 
+    <div>Ajout Article</div>
+    <form>
+        <div>
+            <label>identifiant</label>
+            <input type="text" />
+        </div>
+        <div>
+            <label>référence</label>
+            <input type="text" />
+        </div>
+        <div>
+            <label>prix unitaire</label>
+            <input type="text" />
+        </div>
+        <div>
+            <input type="button" value="Retour" />
+            <input type="submit" value="Ajouter" />
+        </div>
+    </form>
+    </>;
 };
 
 export default AddArticle;
