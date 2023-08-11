87939b71448c8a74d824dd4270754f205ace0049
David Matoska <david.matoska@gmail.com>
2023-08-11 13:15:22 +0200

    commit

--- src/components/AddArticle.tsx
+++ src/components/AddArticle.tsx
@@ -1,31 +1,57 @@
+import { useState } from "react";
+import "./ArticleForm.style.css";
 
 type Props = {
     onBackBtnClickHnd : ()  => void
 }
 
 const AddArticle = (props : Props) => {
-        const { onBackBtnClickHnd } = props
-    return <> 
-    <div>Ajout Article</div>
+
+        const [id, setId] = useState("")
+
+        const [reference, setReference] = useState("")
+
+        const [price, setPrice] = useState("")
+
+        const { onBackBtnClickHnd } = props;
+
+        const onIdChangeHnd = (e: any) => {
+            setId(e.target.value)
+        }
+
+        const onReferenceChangeHnd = (e: any) => {
+            setReference(e.target.value)
+        }
+
+        const onPriceChangeHnd = (e: any) => {
+            setPrice(e.target.value)
+        }
+
+    return( 
+    <div className="form-container">
+    <div>
+        <h3>Ajout Article</h3>
+    </div>
     <form>
         <div>
             <label>identifiant</label>
-            <input type="text" />
+            <input type="text" value={id} onChange={onIdChangeHnd} />
         </div>
         <div>
             <label>référence</label>
-            <input type="text" />
+            <input type="text" value={reference} onChange={onReferenceChangeHnd} />
         </div>
         <div>
             <label>prix unitaire</label>
-            <input type="text" />
+            <input type="text" value={price} onChange={onPriceChangeHnd} />
         </div>
         <div>
             <input type="button" value="Retour" onClick={onBackBtnClickHnd} />
             <input type="submit" value="Ajouter" />
         </div>
     </form>
-    </>;
+    </div>
+    )
 };
 
 export default AddArticle;
