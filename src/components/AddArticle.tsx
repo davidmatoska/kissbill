bdd5f368b807ed0589d0bf7af1b3e5639a189a33
David Matoska <david.matoska@gmail.com>
2023-08-15 12:56:03 +0200

    commit

--- src/components/AddArticle.tsx
+++ src/components/AddArticle.tsx
@@ -5,35 +5,35 @@ import ArticleList from "../models/ArticleList";
 
 
 type Props = {
-    onBackBtnClickHnd : ()  => void;
+    onBackBtnClickHnd: () => void;
     onSubmitClickHnd: (data: IArticle) => void
 };
 
-const AddArticle = (props : Props) => {
+const AddArticle = (props: Props) => {
 
-        const [id, setId] = useState("")
+    const [id, setId] = useState("");
 
-        const [reference, setReference] = useState("")
+    const [reference, setReference] = useState("");
 
-        const [price, setPrice] = useState("")
+    const [price, setPrice] = useState("");
 
-        const { onBackBtnClickHnd, onSubmitClickHnd } = props;
+    const { onBackBtnClickHnd, onSubmitClickHnd } = props;
 
-        const onIdChangeHnd = (e: any) => {
-            setId(e.target.value)
-        }
+    const onIdChangeHnd = (e: any) => {
+        setId(e.target.value)
+    };
 
-        const onReferenceChangeHnd = (e: any) => {
-            setReference(e.target.value)
-        }
+    const onReferenceChangeHnd = (e: any) => {
+        setReference(e.target.value)
+    };
 
-        const onPriceChangeHnd = (e: any) => {
-            setPrice(e.target.value)
-            
-        }
+    const onPriceChangeHnd = (e: any) => {
+        setPrice(e.target.value)
 
-        const onSubmitBtnClickHnd = (e: any) => {
-            e.preventDefault();
+    };
+
+    const onSubmitBtnClickHnd = (e: any) => {
+        e.preventDefault();
         const data: IArticle = {
             id: id,
             reference: reference,
@@ -43,33 +43,33 @@ const AddArticle = (props : Props) => {
         onSubmitClickHnd(data);
         onBackBtnClickHnd();
 
-        }
+    }
 
 
-    return( 
-    <div className="form-container">
-    <div>
-        <h3>Ajout Article</h3>
-    </div>
-    <form onSubmit={onSubmitBtnClickHnd}>
-        <div>
-            <label>identifiant</label>
-            <input type="text" value={id} onChange={onIdChangeHnd} />
+    return (
+        <div className="form-container">
+            <div>
+                <h3>Ajout Article</h3>
+            </div>
+            <form onSubmit={onSubmitBtnClickHnd}>
+                <div>
+                    <label>identifiant</label>
+                    <input type="text" value={id} onChange={onIdChangeHnd} />
+                </div>
+                <div>
+                    <label>référence</label>
+                    <input type="text" value={reference} onChange={onReferenceChangeHnd} />
+                </div>
+                <div>
+                    <label>prix unitaire</label>
+                    <input type="number" pattern="[0-9]*" value={price} onChange={onPriceChangeHnd} />
+                </div>
+                <div>
+                    <input type="button" value="Retour" onClick={onBackBtnClickHnd} />
+                    <input type="submit" value="Ajouter" />
+                </div>
+            </form>
         </div>
-        <div>
-            <label>référence</label>
-            <input type="text" value={reference} onChange={onReferenceChangeHnd} />
-        </div>
-        <div>
-            <label>prix unitaire</label>
-            <input type="number" pattern="[0-9]*" value={price} onChange={onPriceChangeHnd} />
-        </div>
-        <div>
-            <input type="button" value="Retour" onClick={onBackBtnClickHnd} />
-            <input type="submit" value="Ajouter" />
-        </div>
-    </form>
-    </div>
     )
 };
 
